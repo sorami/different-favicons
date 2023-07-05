dd23d2c1dfe714dcb7c029915fcb58cbb0aef7ab
Sorami Hisamoto <hisamoto@mierune.co.jp>
2023-07-05 11:03:33 +0900

    Favicons for each environment

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+const faviconDict: Record<string, string> = {
+  "development": "🚧",
+  "staging": "🚨",
+  "production": "🥳",
+};
+
+const updateFavicon = (char: string) => {
+  if (!char) return;
+  const head = document.querySelector("head");
+  head?.append(
+    Object.assign(document.createElement("link"), {
+      rel: "icon",
+      href: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${char}</text></svg>`,
+    })
+  );
+};
+updateFavicon(faviconDict[import.meta.env.MODE] ?? "");
 </script>
 
 <template>
